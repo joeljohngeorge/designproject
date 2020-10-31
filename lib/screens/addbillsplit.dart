c09dec30cb1d480dcad3111d8175481d5d55966e
Joel John George <58634455+joeljohngeorge@users.noreply.github.com>
2020-10-31 20:27:51 +0530

    Expense add ui

---
+++ lib/screens/addbillsplit.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class Billsplitaddpage extends StatefulWidget {
+  @override
+  _BillsplitaddpageState createState() => _BillsplitaddpageState();
+}
+
+class _BillsplitaddpageState extends State<Billsplitaddpage> {
+  var _categories = [
+    'Finance',
+     'Education',
+    'Groceries',
+    'Food & Restaurants',
+    'Health',
+    'Entertainment',
+    'Shopping',
+    'Home & Utilities',
+    'Electricity Bill',
+    'Water Bill',
+    'Others'
+  ];
+  var _currentitemselected = 'Finance';
+  final _billamtcon = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Enter Bill splitting details'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+                child: Column(children: [
+              TextFormField(
+                style: TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                    icon: const Icon(Icons.monetization_on),
+                    labelText: ' Total Amount',
+                    labelStyle: TextStyle(fontSize: 18)),
+                validator: (val) {
+                  Pattern pattern = r'^[1-9]\d*(\.\d+)?$';
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(val))
+                    return 'Enter a valid number';
+                  else
+                    return null;
+                },
+                keyboardType: TextInputType.number,
+                controller: _billamtcon,
+              ),
+              TextFormField(
+                style: TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.group),
+                  hintText: 'Enter no.of persons',
+                  labelText: 'Friends',
+                  labelStyle: TextStyle(fontSize: 18),
+                ),
+                validator: (val) {
+                  Pattern pattern = r'^[1-9]\d*(\.\d+)?$';
+
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(val)) {
+                    return 'Enter a valid number';
+                  } else {
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.number,
+              ),
+              DropdownButton<String>(
+                items: _categories.map((String dropDownStringItem) {
+                  return DropdownMenuItem<String>(
+                    value: dropDownStringItem,
+                    child: Text(dropDownStringItem),
+                  );
+                }).toList(),
+                onChanged: (String newValueSelected) {
+                  _onDropDownItemSelected(newValueSelected);
+                },
+                value: _currentitemselected,
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                onPressed: () {},
+                child: new Text('Submit'),
+              )
+            ]))));
+  }
+
+  void _onDropDownItemSelected(String newValueSelected) {
+    setState(() {
+      this._currentitemselected = newValueSelected;
+    });
+  }
+}
---
+++ lib/screens/expenseaddpage.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class Expenseaddpage extends StatefulWidget {
+  @override
+  _ExpenseaddpageState createState() => _ExpenseaddpageState();
+}
+
+class _ExpenseaddpageState extends State<Expenseaddpage> {
+  var _categories = [
+    'Finance',
+     'Education',
+    'Groceries',
+    'Food & Restaurants',
+    'Health',
+    'Entertainment',
+    'Shopping',
+    'Home & Utilities',
+    'Electricity Bill',
+    'Water Bill',
+    'Others'
+  ];
+  var _currentitemselected = 'Finance';
+  final _expamtcon = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Enter expense details'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+                child: Column(children: [
+              TextFormField(
+                style: TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                    icon: const Icon(Icons.monetization_on),
+                    labelText: 'Amount',
+                    labelStyle: TextStyle(fontSize: 18)),
+                validator: (val) {
+                  Pattern pattern = r'^[1-9]\d*(\.\d+)?$';
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(val))
+                    return 'Enter a valid number';
+                  else
+                    return null;
+                },
+                keyboardType: TextInputType.number,
+                controller: _expamtcon,
+              ),
+              TextFormField(
+                style: TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.calendar_today),
+                  hintText: 'Enter date(yyyy-mm-dd)',
+                  labelText: 'Date',
+                  labelStyle: TextStyle(fontSize: 18),
+                ),
+                validator: (val) {
+                  Pattern pattern =
+                      r'^((?:19|20)\d\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$';
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(val)) {
+                    return 'Enter a valid date';
+                  } else {
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.datetime,
+              ),
+              DropdownButton<String>(
+                items: _categories.map((String dropDownStringItem) {
+                  return DropdownMenuItem<String>(
+                    value: dropDownStringItem,
+                    child: Text(dropDownStringItem),
+                  );
+                }).toList(),
+                onChanged: (String newValueSelected) {
+                  _onDropDownItemSelected(newValueSelected);
+                },
+                value: _currentitemselected,
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                onPressed: () {},
+                child: new Text('Submit'),
+              )
+            ]))));
+  }
+
+  void _onDropDownItemSelected(String newValueSelected) {
+    setState(() {
+      this._currentitemselected = newValueSelected;
+    });
+  }
+}
