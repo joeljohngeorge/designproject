f3d9be7aa98cbc8f639a8ad85d231a5774c4683d
Joel John George <58634455+joeljohngeorge@users.noreply.github.com>
2020-11-03 20:35:57 +0530

    Not working as expected

--- lib/screens/billsplitting.dart
+++ lib/screens/billsplitting.dart
@@ -1,3 +1,4 @@
+import 'package:expensemonitor/screens/addbillsplit.dart';
 import 'package:flutter/material.dart';
 
 class Billsplitting extends StatefulWidget {
@@ -15,6 +16,47 @@ class _BillsplittingState extends State<Billsplitting> {
               fontSize: 18,
             )),)
       ),
+       floatingActionButton: FloatingActionButton(
+        child:Icon(Icons.add) ,
+          tooltip: "Split bill",
+          onPressed: (){
+            Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Billsplitaddpage()),
+        );
+         }),
+       body:DataTable(
+      
+         columns: const <DataColumn>[
+       DataColumn(
+         label: Text(
+           'Category',
+           style: TextStyle(fontStyle: FontStyle.italic),
+         ),
+       ),
+       DataColumn(
+         label: Text(
+           'Total',
+           style: TextStyle(fontStyle: FontStyle.italic),
+         ),
+       ),
+       DataColumn(
+         label: Text(
+             'Friends',
+             style: TextStyle(fontStyle: FontStyle.italic),
+           ),
+       ),
+      
+      ], 
+      //Expected  data
+         rows: const <DataRow> [
+         DataRow(cells: [
+        DataCell(Text('Entertainment')),
+        DataCell(Text('1200')),
+        DataCell(Text('4')),
+        
+         ]),] )
+         
     );
       
     
