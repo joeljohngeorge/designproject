1d0020ecb6e41463826212c4579105f7d0a8f3e5
Joel John George <58634455+joeljohngeorge@users.noreply.github.com>
2020-11-03 20:42:24 +0530

    Added DataTable

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,10 @@
 import 'package:expensemonitor/screens/analysispage.dart';
 import 'package:expensemonitor/screens/billsplitting.dart';
 import 'package:expensemonitor/screens/budgetplanning.dart';
+import 'package:expensemonitor/screens/expenseaddpage.dart';
+
 import 'package:flutter/material.dart';
+void main()=>runApp(Homepage());
 
 class Homepage extends StatefulWidget {
   @override
@@ -21,6 +24,50 @@ class _HomepageState extends State<Homepage> {
                 ))),
       ),
       drawer: Navdrawer(),
+      floatingActionButton: FloatingActionButton(
+        child:Icon(Icons.add) ,
+          tooltip: "Add expense",
+          onPressed: (){
+            Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Expenseaddpage()),
+        );
+
+          }),
+     body:SingleChildScrollView(
+            child: Container(
+               height: 600,
+           width: MediaQuery.of(context).size.width,
+              child: DataTable(
+           columns:  <DataColumn>[
+              DataColumn(
+              label: Text(
+                'Category',
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+              ),
+              DataColumn(
+              label: Text(
+                'Amount',
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+              ),
+              DataColumn(
+              label: Text(
+                'Date',
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+              ),
+          ], 
+          //Sample data
+           rows:  <DataRow> [
+                    DataRow(cells: [
+                      DataCell(Text('Finance')),
+                      DataCell(Text('3000')),
+                      DataCell(Text('2020-10-31')),
+                    ]),] ),
+            ),
+     )
     );
   }
 }
@@ -42,7 +89,7 @@ class _NavdrawerState extends State<Navdrawer> {
               style: TextStyle(color: Colors.white, fontSize: 25),
             ),
           ),
-          decoration:  BoxDecoration(color: Colors.blue),
+          decoration: BoxDecoration(color: Colors.blue),
         ),
         ListTile(
           title: Text("Home"),
@@ -58,19 +105,15 @@ class _NavdrawerState extends State<Navdrawer> {
         ListTile(
           title: Text("Analysis & Review"),
           onTap: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Analysispage())
-            )
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => Analysispage()))
           },
         ),
         ListTile(
           title: Text("Bill splitting"),
           onTap: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Billsplitting())
-            )
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => Billsplitting()))
           },
         ),
         ListTile(
