e06ee42e057dc55c657bd86ba3196b4a3186fd60
Joel John George <58634455+joeljohngeorge@users.noreply.github.com>
2020-11-05 23:04:30 +0530

    Alert messages,but not displaying ..pinenthina...

---
+++ lib/alertdialogs.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+
+
+class Alert1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your income "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+class Alert2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in finance category "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+
+class Alert3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in education category "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+
+class Alert4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in groceries "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+class Alert5 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in food&restaurants "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+class Alert6 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in transportation "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+
+class Alert7 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in health "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+
+class Alert8 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in entertainment  "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+
+class Alert9 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in shopping "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+class Alert10 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in home & utilties "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+
+class Alert11 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You are not entered your electricity bill amount. Please enter amount "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+
+class Alert12 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You are not entered your water bill amount. Please enter amount"),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
+class Alert13 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Warning", style: TextStyle(fontWeight: FontWeight.bold)),
+      content: Text("You have used 90% of your money allocated in other categories "),
+      actions: [FlatButton(onPressed: (){}, child: Text("Ok"))],
+      shape: CircleBorder(),
+      
+    );
+  }
+}
