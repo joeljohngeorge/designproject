e1757c06bcb521f67d7b133da7055e5aaba4c84b
Joel John George <58634455+joeljohngeorge@users.noreply.github.com>
2020-11-19 16:45:07 +0530

    No hope

--- lib/screens/expenseaddpage.dart
+++ lib/screens/expenseaddpage.dart
@@ -8,28 +8,32 @@ class Expenseaddpage extends StatefulWidget {
 class _ExpenseaddpageState extends State<Expenseaddpage> {
   var _categories = [
     'Finance',
-     'Education',
+    'Education',
     'Groceries',
-    'Food & Restaurants',
+    'Food',
     'Transportation',
     'Health',
     'Entertainment',
     'Shopping',
-    'Home & Utilities',
+    'Home',
     'Electricity Bill',
     'Water Bill',
     'Others'
   ];
   var _currentitemselected = 'Finance';
   final _expamtcon = TextEditingController();
-  final _datecon=TextEditingController();
-   final formKey = GlobalKey<FormState>(); 
+  final _datecon = TextEditingController();
+  final _descriptioncon = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
-    void _submit() {
-     final form = formKey.currentState; 
-      if (form.validate()) {
-         form.save(); 
-          Navigator.pop(context); }}
+  void _submit() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,68 +43,80 @@ class _ExpenseaddpageState extends State<Expenseaddpage> {
         body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Form(
-              key: formKey,
+                key: formKey,
                 child: Column(children: [
-              TextFormField(
-                style: TextStyle(fontSize: 22),
-                decoration: const InputDecoration(
-                    icon: const Icon(Icons.monetization_on),
-                    labelText: 'Amount',
-                    labelStyle: TextStyle(fontSize: 16)),
-                validator: (val) {
-                  Pattern pattern = r'^[1-9]\d*(\.\d+)?$';
-                  RegExp regex = new RegExp(pattern);
-                  if (!regex.hasMatch(val))
-                    return 'Enter a valid number';
-                  else
-                    return null;
-                },
-                keyboardType: TextInputType.number,
-                controller: _expamtcon,
-              ),
-              TextFormField(
-                style: TextStyle(fontSize: 22),
-                decoration: const InputDecoration(
-                  icon: const Icon(Icons.calendar_today),
-                  hintText: 'Enter date(yyyy-mm-dd)',
-                  labelText: 'Date',
-                  labelStyle: TextStyle(fontSize: 16),
-                ),
-                validator: (val) {
-                  Pattern pattern =
-                      r'^((?:19|20)\d\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$';
-                  RegExp regex = new RegExp(pattern);
-                  if (!regex.hasMatch(val)) {
-                    return 'Enter a valid date';
-                  } else {
-                    return null;
-                  }
-                },
-                keyboardType: TextInputType.datetime,
-                controller: _datecon,
-              
-              ),
-              SizedBox(height: 10,),
-              DropdownButton<String>(
-                items: _categories.map((String dropDownStringItem) {
-                  return DropdownMenuItem<String>(
-                    value: dropDownStringItem,
-                    child: Text(dropDownStringItem),
-                  );
-                }).toList(),
-                onChanged: (String newValueSelected) {
-                  _onDropDownItemSelected(newValueSelected);
-                },
-                value: _currentitemselected,
-              ),
-              SizedBox(height: 20),
-              RaisedButton(
-                onPressed: () {
-                  _submit();
-                },
-                child: new Text('Submit'),
-              )
-            ]))));
+                  TextFormField(
+                    style: TextStyle(fontSize: 22),
+                    decoration: const InputDecoration(
+                        icon: const Icon(Icons.monetization_on),
+                        labelText: 'Amount',
+                        labelStyle: TextStyle(fontSize: 16)),
+                    validator: (val) {
+                      Pattern pattern = r'^[1-9]\d*(\.\d+)?$';
+                      RegExp regex = new RegExp(pattern);
+                      if (!regex.hasMatch(val))
+                        return 'Enter a valid number';
+                      else
+                        return null;
+                    },
+                    keyboardType: TextInputType.number,
+                    controller: _expamtcon,
+                  ),
+                  TextFormField(
+                    style: TextStyle(fontSize: 22),
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.calendar_today),
+                      hintText: 'Enter date(yyyy-mm-dd)',
+                      labelText: 'Date',
+                      labelStyle: TextStyle(fontSize: 16),
+                    ),
+                    validator: (val) {
+                      Pattern pattern =
+                          r'^((?:19|20)\d\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$';
+                      RegExp regex = new RegExp(pattern);
+                      if (!regex.hasMatch(val)) {
+                        return 'Enter a valid date';
+                      } else {
+                        return null;
+                      }
+                    },
+                    keyboardType: TextInputType.datetime,
+                    controller: _datecon,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  DropdownButton<String>(
+                    items: _categories.map((String dropDownStringItem) {
+                      return DropdownMenuItem<String>(
+                        value: dropDownStringItem,
+                        child: Text(dropDownStringItem),
+                      );
+                    }).toList(),
+                    onChanged: (String newValueSelected) {
+                      _onDropDownItemSelected(newValueSelected);
+                    },
+                    value: _currentitemselected,
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    style: TextStyle(fontSize: 22),
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.category),
+                      labelText: 'Description',
+                      labelStyle: TextStyle(fontSize: 16),
+                    ),
+                    keyboardType: TextInputType.text,
+                    controller: _descriptioncon,
+                  ),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                    onPressed: () {
+                      _submit();
+                    },
+                    child: new Text('Submit'),
+                  )
+                ]))));
   }
 
   void _onDropDownItemSelected(String newValueSelected) {
